0190dade9b84186aa808bea916981ddeed71bcf7
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-03-17 15:11:07 +0530

    create models and configure database

---
+++ Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/model/User.java
@@ -0,0 +1,27 @@
+package com.url.shortener.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private  String username;
+    private  String password;
+    private String role ="ROLE_USER";
+
+
+}
