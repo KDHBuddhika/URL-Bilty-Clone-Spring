736daf0295ab4e97d8583535d21f807d70f1805c
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-03-24 17:53:57 +0530

    Web Security Config class

---
+++ Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.url.shortener.service;
+
+import com.url.shortener.model.User;
+import com.url.shortener.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not founded with username: "+username));
+        return UserDetailsImpl.build(user);
+    }
+}
