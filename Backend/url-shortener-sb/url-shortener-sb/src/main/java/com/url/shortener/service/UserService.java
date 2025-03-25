49e32abd4441e8167297364f039a6685128ec6d2
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-03-25 18:28:43 +0530

    urlmapping controller

--- Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/service/UserService.java
+++ Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/service/UserService.java
@@ -9,4 +9,6 @@ public interface UserService {
     User registerUser(User user);
     
     JwtAuthenticationResponse authenticationUser(LoginRequest loginRequest);
+    
+    User findByUsername(String name);
 }
