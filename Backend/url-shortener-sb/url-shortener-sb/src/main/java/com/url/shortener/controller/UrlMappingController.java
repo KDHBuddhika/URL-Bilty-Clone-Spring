8b41db1f5174d62518458cfc43f9748006c3147a
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-04-02 23:55:55 +0530

    get my urls controller

--- Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/controller/UrlMappingController.java
+++ Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/controller/UrlMappingController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,6 +27,8 @@ public class UrlMappingController {
     private UserService userService;
     
     //{"originalUrl":"http://example.com"}
+    //http://abc.com/SJlx9jkl  --> http://example.com
+    
     @PostMapping("/shorten")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<UrlMappingDTO> createShortUrl(@RequestBody Map<String ,String> request,
@@ -36,6 +39,14 @@ public class UrlMappingController {
          UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl,user);
          return ResponseEntity.ok(urlMappingDTO);
         
+    }
+    
+    
+    
+    @PostMapping("/myurls")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<UrlMappingDTO>> getUserUrls(Principal principal){
+        User user =userService.findByUsername(principal.getName());
     }
     
     
