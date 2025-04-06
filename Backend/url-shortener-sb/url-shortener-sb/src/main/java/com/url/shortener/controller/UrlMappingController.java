fd702aa1fb8dfbfeee082421dddb319e31578432
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-04-06 23:46:40 +0530

    click event

--- Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/controller/UrlMappingController.java
+++ Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/controller/UrlMappingController.java
@@ -1,6 +1,8 @@
 package com.url.shortener.controller;
 
+import com.url.shortener.dtos.ClickEventDTO;
 import com.url.shortener.dtos.UrlMappingDTO;
+import com.url.shortener.model.ClickEvent;
 import com.url.shortener.model.User;
 import com.url.shortener.service.UrlMappingService;
 import com.url.shortener.service.UserService;
@@ -10,6 +12,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -47,6 +51,24 @@ public class UrlMappingController {
         List<UrlMappingDTO>  urls = urlMappingService.getUrlsByUser(user);
         return ResponseEntity.ok(urls);
     }
+    
+    
+    @GetMapping("/analytics/{shortUrl}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<ClickEventDTO>> getUserUrls(@PathVariable String shortUrl,
+                                                           @RequestParam("startDate") String startDate ,
+                                                           @RequestParam("endDate") String endDate){
+        //The ISO format looks like: "2023-12-31T23:59:59"
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        LocalDateTime start = LocalDateTime.parse(startDate , formatter);
+        LocalDateTime end = LocalDateTime.parse(endDate , formatter);
+        List<ClickEventDTO> clickEventDTOS= urlMappingService.getClickEventByDate(shortUrl,start,end);
+        return ResponseEntity.ok(clickEventDTOS);
+        
+        
+    }
+    
+    
     
     
 }
