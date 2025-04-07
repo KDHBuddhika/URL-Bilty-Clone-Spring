5cdbd921ec17f9fc8f2c73c4cacc90919e03c2c0
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-04-07 14:28:30 +0530

    set Click event

--- Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/service/impl/UrlMappingServiceIMPL.java
+++ Backend/url-shortener-sb/url-shortener-sb/src/main/java/com/url/shortener/service/impl/UrlMappingServiceIMPL.java
@@ -108,6 +108,17 @@ public class UrlMappingServiceIMPL implements UrlMappingService {
     @Override
     public UrlMapping getOriginalUrl(String shortUrl) {
         UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        
+        if (urlMapping != null){
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+            
+            //Recode Clickevent
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
         return urlMapping;
     }
 }
