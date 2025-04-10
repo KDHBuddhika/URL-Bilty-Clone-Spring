9555b09f1db28f47852e42cef7b84e4358436b58
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2025-04-11 00:35:32 +0530

    use context , login page , register page

--- url-shortner-frontend/src/App.jsx
+++ url-shortner-frontend/src/App.jsx
@@ -8,6 +8,8 @@ import { Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast'
+import LoginPage from './components/LoginPage'
 
 function App() {
 
@@ -16,11 +18,14 @@ function App() {
     <>
     
      <Router>
-      {/* <Navbar/> */}
+      <Navbar/>
+      <Toaster position='bottom-center'/>
           <Routes>
               <Route path="/" element={<LandingPage />} /> 
               <Route path="/about" element={<AboutPage />} /> 
               <Route path="/register" element={<RegisterPage />} /> 
+              <Route path="/login" element={<LoginPage />} /> 
+
           </Routes> 
 
         <Footer/>
